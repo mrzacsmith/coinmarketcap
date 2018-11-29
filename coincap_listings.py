8f580daaa3aaeeb526bf4aea856c05e64bfdb2b3
Zac Smith <zrs3141592@gmail.com>
2018-11-29 20:16:19 +0000

    prints id, name and symbol

--- coincap_listings.py
+++ coincap_listings.py
@@ -6,4 +6,12 @@ listings_url = 'https://api.coinmarketcap.com/v2/listings/'
 request = requests.get(listings_url)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+# print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results['data']
+
+for currency in data:
+    rank = currency['id']
+    name = currency['name']
+    symbol = currency['symbol']
+    print(str(rank) + ': ' + name + '(' + symbol + ')' )
