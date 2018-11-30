dec643ed992d6d1fb81196e9ed3b9c99b9fd47bf
Zac Smith <zrs3141592@gmail.com>
2018-11-30 00:19:16 +0000

    use format data from coincap_ticker

--- coincap_ticker_specific.py
+++ coincap_ticker_specific.py
@@ -4,11 +4,12 @@ import requests
 convert = 'USD'
 
 listings_url = 'https://api.coinmarketcap.com/v2/listings/'
+url_end = '?structure=array&convert=' + convert
 
 request = requests.get(listings_url)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+# print(json.dumps(results, sort_keys=True, indent=4))
 
 data = results['data']
 ticker_url_pairs = {}
@@ -18,11 +19,60 @@ for currency in data:
     url = currency['id']
     ticker_url_pairs[symbol] = url
 
-print(ticker_url_pairs)
+# print(ticker_url_pairs)
 
-# ticker_specific_url = 'https://api.coinmarketcap.com/v2/ticker/1/'
+while True:
+    print
+    choice = input("Enter the ticker symbol of a cryptocurrency: ")
+    choice = choice.upper()
+    print(choice)
 
-# request = requests.get(ticker_specific_url)
-# results = request.json()
+    ticker_specific_url = 'https://api.coinmarketcap.com/v2/ticker/' + str(ticker_url_pairs[choice]) + '/' + url_end
+    
+    request = requests.get(ticker_specific_url)
+    results = request.json()
+    
+    # print(json.dumps(results, sort_keys=True, indent=4))
 
-# print(json.dumps(results, sort_keys=True, indent=4))
+    currency = results['data'][0]
+    
+    rank = currency['rank']
+    name = currency['name']
+    symbol = currency['symbol']
+        
+    circulating_supply = int(currency['circulating_supply'])
+    total_supply = int(currency['total_supply'])
+        
+        
+    quotes = currency['quotes'][convert]
+    market_cap = int(quotes['market_cap'])
+    hour_change = quotes['percent_change_1h']
+    day_change = quotes['percent_change_24h']
+    week_change = quotes['percent_change_7d']
+    price = quotes['price']
+    volume = quotes['volume_24h']
+        
+    volume_string = '{:,}'.format(volume)
+    market_cap_string = '{:,}'.format(market_cap)
+    circulating_supply_string = '{:,}'.format(circulating_supply)
+    total_supply_string = '{:,}'.format(total_supply)
+        
+    print
+    print(str(rank) + ': ' + name + ' (' + symbol + ')' )
+    print('Market cap: \t\t$' + market_cap_string)
+    print('Price: \t\t\t$' + str(price))
+    print('24h Volume: \t\t$' + volume_string)
+    print('Hour change: \t\t' + str(hour_change) + '%')
+    print('Day change: \t\t' + str(day_change) + '%')
+    print('Week change: \t\t' + str(week_change) + '%')
+    print('Total supply: \t\t' + total_supply_string)
+    print('Circulating supply: \t' + circulating_supply_string)
+    print('Percentage of coins in circulation: ' + str(float(circulating_supply / total_supply * 100)))
+    print
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        
+        
+    choice = input('Run again? (y/n) ')
+
+    if choice == 'n':
+        break
