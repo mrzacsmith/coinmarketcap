54fd42145f0ea950028c6536b7c25045b6489cd6
Zac Smith <zrs3141592@gmail.com>
2018-11-30 00:01:47 +0000

    make dictionary to store ticker symbols

--- coincap_ticker_specific.py
+++ coincap_ticker_specific.py
@@ -1,9 +1,28 @@
 import json
 import requests
 
-ticker_specific_url = 'https://api.coinmarketcap.com/v2/ticker/1/'
+convert = 'USD'
 
-request = requests.get(ticker_specific_url)
+listings_url = 'https://api.coinmarketcap.com/v2/listings/'
+
+request = requests.get(listings_url)
 results = request.json()
 
 print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results['data']
+ticker_url_pairs = {}
+
+for crap in data:
+    symbol = crap['symbol']
+    url = crap['id']
+    ticker_url_pairs[symbol] = url
+
+print(ticker_url_pairs)
+
+# ticker_specific_url = 'https://api.coinmarketcap.com/v2/ticker/1/'
+
+# request = requests.get(ticker_specific_url)
+# results = request.json()
+
+# print(json.dumps(results, sort_keys=True, indent=4))
